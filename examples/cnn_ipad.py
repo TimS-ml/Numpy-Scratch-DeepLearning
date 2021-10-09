1b10db1dc1a8ea7d5633e8fe0cbb267464249bc5
TimS <jianshen@pku.edu.cn>
2021-10-09 05:58:16 -0400

    Commit on 2021-10-09 05:58

--- examples/cnn_ipad.py
+++ examples/cnn_ipad.py
@@ -1,3 +1,14 @@
 from sys import path as syspath
 from os import path as ospath
 syspath.append(ospath.join(ospath.expanduser('~'), 'scratchDL'))
+
+import numpy as np
+import gzip
+import requests
+import io
+
+url = 'https://github.com/scikit-learn/scikit-learn/blob/main/sklearn/datasets/data/digits.csv.gz?raw=true'
+
+f = requests.get(url).content
+data = np.loadtxt(gzip.open(io.BytesIO(f), 'rt'),
+                  delimiter=',', dtype=np.float32)
